9cecba70040dafbbe8081ecf98c40dd5fcda689b
Jacob Kagon <jacob@kagon.net>
2020-09-20 02:28:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ end
 def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   emoticon_lib.each do |meaning, value|
-  binding.pry
+  #binding.pry
     if emoticon == value[:japanese]
       return meaning
     end
