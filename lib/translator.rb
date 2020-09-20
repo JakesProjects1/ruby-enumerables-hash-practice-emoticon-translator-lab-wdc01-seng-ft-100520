e6d936f656daeebe6ae322b62fdda89e62ec0fb2
Jacob Kagon <jacob@kagon.net>
2020-09-20 02:34:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
     return value[:japanese]
    end
   end
-   
+  "Sorry, that emoticon was not found" 
 end
 
 def get_english_meaning(emoticon_file, emoticon)
